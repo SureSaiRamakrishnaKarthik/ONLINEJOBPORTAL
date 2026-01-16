bfb3fd5835f805383f63917ecb9dd168a5c87222
Karthik <karthiiiispamz@gmail.com>
2026-01-17 01:35:53 +0530

    Add Render frontend URL to CORS

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://onlinejobportal-1.onrender.com",
     "https://onlinejobportal-red.vercel.app",
     "https://onlinejobportal.vercel.app",
     "https://onlinejobportal-git-main.vercel.app",
