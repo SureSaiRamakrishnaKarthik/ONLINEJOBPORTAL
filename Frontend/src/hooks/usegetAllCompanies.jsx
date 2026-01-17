adf6f54a492f00a0a664f2c074cc6b35b1bee5a5
Karthik <karthiiiispamz@gmail.com>
2026-01-17 17:35:37 +0530

    Guard companies fetch to authenticated recruiters; include user in deps

--- Frontend/src/hooks/usegetAllCompanies.jsx
+++ Frontend/src/hooks/usegetAllCompanies.jsx
@@ -1,27 +1,31 @@
 import { setCompanies } from "@/redux/companyslice";
 import { COMPANY_API_ENDPOINT } from "@/utils/data";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllCompanies = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((store) => store.auth);
+
   useEffect(() => {
     const fetchCompanies = async () => {
       try {
         const res = await axios.get(`${COMPANY_API_ENDPOINT}/get`, {
           withCredentials: true,
         });
-        console.log("called");
         if (res.data.success) {
           dispatch(setCompanies(res.data.companies));
         }
       } catch (error) {
-        console.log(error);
+        console.log("Companies fetch error:", error);
       }
     };
-    fetchCompanies();
-  }, []);
+
+    if (user && user.role === "Recruiter") {
+      fetchCompanies();
+    }
+  }, [dispatch, user]);
 };
 
 export default useGetAllCompanies;
